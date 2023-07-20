3b03e20dc039590f98d3dfb047f1edb4ea6f1530
Charbel Allawi <charbelallawiwork@gmail.com>
2023-07-20 10:40:48 +0300

    database connected

--- signup.php
+++ signup.php
@@ -2,23 +2,24 @@
 include('connection.php');
 
 
-$username = $_POST['username'];
+$email = $_POST['email'];
 // $username=chris
 // $password=chris1234
-$password = $_POST['password'];
+
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$check_email = $mysqli->prepare('select email from users where email=?');
+$check_email->bind_param('s', $email);
+$check_email->execute();
+$check_email->store_result();
+$email_exists = $check_email->num_rows();
 
-$check_username = $mysqli->prepare('select username from users where username=?');
-$check_username->bind_param('s', $username);
-$check_username->execute();
-$check_username->store_result();
-$username_exists = $check_username->num_rows();
-
-if ($username_exists == 0) {
+if ($email_exists == 0) {
     $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-    $query = $mysqli->prepare('insert into users(username,password,first_name,last_name) values(?,?,?,?)');
-    $query->bind_param('ssss', $username, $hashed_password, $first_name, $last_name);
+    $query = $mysqli->prepare('insert into users(first_name,last_name,email,password) values(?,?,?,?)');
+    $query->bind_param('ssss', $first_name, $last_name, $email, $hashed_password);
     $query->execute();
 
     $response['status'] = "success";
