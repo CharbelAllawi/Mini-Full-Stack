e79519be7a94d7ffae5f178c4c6138b898d05949
Charbel Allawi <charbelallawiwork@gmail.com>
2023-07-21 09:55:03 +0300

    sign up php

---
+++ assets/php/signup.php
@@ -0,0 +1,31 @@
+<?php
+include('connection.php');
+
+
+$email = $_POST['email'];
+// $username=chris
+// $password=chris1234
+
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$check_email = $mysqli->prepare('select email from users where email=?');
+$check_email->bind_param('s', $email);
+$check_email->execute();
+$check_email->store_result();
+$email_exists = $check_email->num_rows();
+
+if ($email_exists == 0) {
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+    $query = $mysqli->prepare('insert into users(first_name,last_name,email,password) values(?,?,?,?)');
+    $query->bind_param('ssss', $first_name, $last_name, $email, $hashed_password);
+    $query->execute();
+
+    $response['status'] = "success";
+    $response['message'] = "another message in success";
+} else {
+    $response['status'] = "failed";
+    $response['message'] = "another message in fail";
+}
+echo json_encode($response);
